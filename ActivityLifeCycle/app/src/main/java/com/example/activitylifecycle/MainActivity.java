167bf8fcc32b6a03f2a8b1488e44299fa437f9b9
Jugesh Kumar <jugeshsm@gmail.com>
2023-08-15 19:03:50 +0530

    CheckBox

--- ActivityLifeCycle/app/src/main/java/com/example/activitylifecycle/MainActivity.java
+++ ActivityLifeCycle/app/src/main/java/com/example/activitylifecycle/MainActivity.java
@@ -10,6 +10,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +24,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CheckBox checkBox=findViewById(R.id.checkBox);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    Toast.makeText(MainActivity.this, "The checkbox is checked", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Not Checked", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
 
         button=findViewById(R.id.button);
