55d1607089c499fdf2db86ffc5f6a8319a537879
Jugesh Kumar <jugeshsm@gmail.com>
2023-08-17 08:28:11 +0530

    greeting app

--- GreetingsApp/app/src/main/java/com/example/greetingsapp/MainActivity.kt
+++ GreetingsApp/app/src/main/java/com/example/greetingsapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.greetingsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val et:EditText=findViewById(R.id.editText);
+        val btn:Button=findViewById(R.id.button);
+
+
+
+        btn.setOnClickListener(){
+            val str=et.text;
+            Toast.makeText(this,"Hello "+str,Toast.LENGTH_LONG).show();
+        }
     }
 }
