7d695dfe1ead64d987a429263131deb5e9a9c28a
Jugesh Kumar <jugeshsm@gmail.com>
2023-09-03 19:35:09 +0530

    Data Binding

--- DataBindingApp/app/src/main/java/com/example/databindingapp/MainActivity.kt
+++ DataBindingApp/app/src/main/java/com/example/databindingapp/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.databindingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.databindingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+
+//        binding.button.setOnClickListener(){
+//            val str:String=binding.editText.text.toString()
+//            binding.textView.setText("Hello $str")
+//
+//        }
+
+        binding.apply {
+            button.setOnClickListener(){
+                val str:String=editText.text.toString()
+                textView.setText("Hello $str")
+            }
+        }
+
+
     }
 }
